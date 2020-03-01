334b0615ae9a160b58fd7ceb31be2613c634bfc7
haiyan.lin <835084497@qq.com>
2020-03-01 11:55:55 +0800

    update ProblemImage

--- src/components/store/StoreManage.jsx
+++ src/components/store/StoreManage.jsx
@@ -9,8 +9,11 @@ import {
   Alert,
   Radio,
   DatePicker,
-  Select
+  Select,
+  Upload,
+  message
 } from "antd";
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 import moment from "moment";
 import { Api } from "../.././server/_ajax.js";
 import { objToArray } from "../.././server/objtoArray";
@@ -674,6 +677,17 @@ class StoreManage extends Component {
           })}
         </Select>
       </FormItem>
+      <FormItem
+        {...formItemLayout}
+        // validateStatus={reason_status}
+        label={
+          <span>
+            问题图片
+          </span>
+        }>
+        <ProblemImage/>
+      </FormItem>
+      
       </> : null}
       {noChoice ? <Alert
             message={this.state.message}
@@ -1178,6 +1192,63 @@ class StoreManage extends Component {
         {this.ckReplyModal()}
         {this.ckReplyCheckModal()}
       </div>
+    );
+  }
+}
+
+class ProblemImage extends React.Component {
+  state = {
+    loading: false,
+  };
+
+  getBase64(img, callback) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+  }
+
+  beforeUpload(file) {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    if (!isJpgOrPng) {
+      message.error('You can only upload JPG/PNG file!');
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error('Image must smaller than 2MB!');
+    }
+    return isJpgOrPng && isLt2M;
+  }
+
+  handleChange = info => {
+    if (info.file.status === 'uploading') {
+      this.setState({ loading: true });
+      return;
+    }
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      this.getBase64(info.file.originFileObj, imageUrl =>
+        this.setState({
+          imageUrl,
+          loading: false,
+        }),
+      );
+    }
+  };
+
+  render() {
+    const { imageUrl } = this.state;
+    return (
+      <Upload
+        name="avatar"
+        listType="picture-card"
+        className="avatar-uploader"
+        showUploadList={false}
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        beforeUpload={this.beforeUpload}
+        onChange={this.handleChange}
+      >
+        {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : '上传'}
+      </Upload>
     );
   }
 }
